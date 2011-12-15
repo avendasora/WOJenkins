ad40c85e6b708a3fc6e87b887efb8a3b07ee3fad
David Avendasora <avendasorada@me.com>
2011-12-15 12:30:16 +0800

    surround git command with parenthesis

--- Install/Jenkins/jobs/cloneJobDefinition.sh
+++ Install/Jenkins/jobs/cloneJobDefinition.sh
@@ -30,7 +30,7 @@ fi
 # Clone the Job Definition
 if [ ! -e "${JENKINS_HOME}/jobs/${JOB_NAME}" ]; then
     echo "${GIT_INSTALL_DIR}/git clone git://github.com/avendasora/${JOB_DEFINITION} ${JENKINS_HOME}/jobs/${JOB_NAME}"
-	git clone git://github.com/avendasora/${JOB_DEFINITION} ${JENKINS_HOME}/jobs/${JOB_NAME}
+	(git clone git://github.com/avendasora/${JOB_DEFINITION} ${JENKINS_HOME}/jobs/${JOB_NAME})
 	curl -O ${JENKINS_URL}/reload
 else
 	echo "A job already exists with the name ${JOB_NAME}. You must delete the existing job, or chose a different name."
