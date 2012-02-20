eabb93e9a19b131b73f532a46c097d6634bd2bdd
Pascal Robert <probert@macti.ca>
2012-02-20 10:55:23 -0500

    Have to change the deployed name

--- Build/WonderProjects/WorkspaceSetupScripts/setupWonderProjectWorkspace.sh
+++ Build/WonderProjects/WorkspaceSetupScripts/setupWonderProjectWorkspace.sh
@@ -156,6 +156,7 @@ PROJECTS=`ls ${WORKSPACE}/Projects/`
 # Step through them to get the list of WO frameworks on their Classpath.
 for PROJECT in $PROJECTS; do
 	PROJECT_NAME=`cat ${WORKSPACE}/Projects/${PROJECT}/build.properties | sed -n -e 's_.*project\.name=\(.*\).*_\1_p'`
+	DEPLOYED_APPLICATION_NAME="${PROJECT_NAME}"
 	echo "Project name from build.properties : ${PROJECT_NAME}"
 	if [ "${PROJECT}" == "${PROJECT_NAME}" ]; then
 		echo " "
